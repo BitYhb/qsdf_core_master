454c7d3fd13d2c4eb58a41729bf4f4a63240eb78
Worker <Worker>
2023-01-11 22:21:03 +0800

    CMAKE: 添加FindNpm模块

---
+++ cmake/FindNpm.cmake
@@ -0,0 +1,27 @@
+# - Try to find Npm
+# - Copied from one of cmake templates
+# -------------------------
+# Once done this will define
+#
+#  NPM_FOUND - system has Npm
+#  NPM_EXECUTABLE - the Npm executable
+
+set(_doc "Npm command line executable")
+
+set(Npm_ROOT_PATH "C:/Program Files/nodejs" CACHE STRING "Npm executable path prefix")
+
+set(Npm_NAMES npm)
+if(WIN32)
+    set(Npm_NAMES npm.cmd)
+endif()
+
+find_program(Npm_EXECUTABLE NAMES ${Npm_NAMES}
+    HINTS ${Npm_ROOT_PATH}
+    DOC ${_doc})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Npm
+    FOUND_VAR Npm_FOUND
+    REQUIRED_VARS Npm_EXECUTABLE)
+
+unset(_doc)
