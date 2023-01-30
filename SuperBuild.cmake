3d6e18921b4e3356cdbdd961c0bc940f58829838
Worker <zhangxin@truesight.com.cn>
2023-01-30 17:16:58 +0800

    CMAKE: 删除多余代码

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -34,12 +34,6 @@ list(TRANSFORM core_defines REPLACE "\"" "\\\\\"" OUTPUT_VARIABLE core_defines)
 get_property(core_public_defines GLOBAL PROPERTY Core_DELAY_ADD_COMPILE_DEFINITIONS_PUBLIC)
 list(TRANSFORM core_public_defines REPLACE "\"" "\\\\\"" OUTPUT_VARIABLE core_public_defines)
 
-get_property(core_defines GLOBAL PROPERTY Core_DELAY_ADD_COMPILE_DEFINITIONS_PRIVATE)
-list(TRANSFORM core_defines REPLACE "\"" "\\\\\"" OUTPUT_VARIABLE core_defines)
-
-get_property(core_public_defines GLOBAL PROPERTY Core_DELAY_ADD_COMPILE_DEFINITIONS_PUBLIC)
-list(TRANSFORM core_public_defines REPLACE "\"" "\\\\\"" OUTPUT_VARIABLE core_public_defines)
-
 ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     DEPENDS ${qsdf-core_DEPENDENCIES}
